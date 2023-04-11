8d6ad892ec22a46fc9cdc32d2e156ce517c05787
Christopher Bruisma <63408252+KodakC41@users.noreply.github.com>
2023-04-11 16:38:00 -0400

    Added More functionality

--- Test.py
+++ Test.py
@@ -7,7 +7,7 @@ import imageio as iio
 import sklearn.decomposition
 import numpy as np
 
-img = "/Users/chris/Documents/UR/SENIOR/FinalSemester/CSC249/Final/JubPalProcess/Court_stylized_BubbleWrape.jpg"
+img = "/Users/chris/Documents/UR/SENIOR/FinalSemester/CSC249/Final/JubPalProcess/Cactus.jpeg"
 
 def blurdivide(img,sigma):
 	if not img.dtype == "float32":
@@ -21,30 +21,36 @@ def blurdivide(img,sigma):
 img = photo.imread(img)
 sigma = ndimage.standard_deviation(img)
 bd = blurdivide(img=img,sigma=sigma)
-# final_step =  bd / img
+final_step =  bd / img
 # photo.imwrite("blurdiv.png",final_step)
 
 
 
 def rgb2gray(rgb):
     return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
-dim_2 = img
+dim_2 = rgb2gray(img)
+
+import skimage.io as io 
+import numpy as np 
+from sklearn.decomposition import KernelPCA 
 
 def K_PCA(img):
-	img = np.array(img, dtype='float32') 
-	row = img.shape[0] 
-	col = img.shape[1] 
-	bands = img.shape[2] 
-	ras_shape = (row * col, bands)
-	img_array = img[:, :, :bands].reshape(ras_shape) 
-	kpca = sklearn.decomposition.KernelPCA(n_components=4, 
+    img = np.array(img, dtype='float32') 
+    # Poly is good
+    row = img.shape[0] 
+    col = img.shape[1] 
+    print(img.size,row,col)
+    ras_shape = (row * col, 0) 
+    pca = KernelPCA(n_components=759, 
                             kernel="poly", 
-                            fit_inverse_transform=False, 
-                            gamma=0.5).fit_transform(img_array)
-	print("success")
-	new_shape = (4, col, row) 
-	kpca = kpca.reshape(new_shape)
-	photo.imwrite("KPCA_S_Court.png",kpca)
-K_PCA(dim_2)
+                            fit_inverse_transform=False, eigen_solver="dense",
+                            gamma=2).fit_transform(img) 
+    print(pca.size,pca.shape[0],pca.shape[1])
+    new_shape = (pca.shape[0], pca.shape[1], -1) 
+    kpca = pca.reshape(new_shape) 
+    # saved_image = "kernel_pca.tif"
+    io.imsave("Kernel_PCA.tif", kpca)
+    return kpca
+photo.imwrite("KPCA_over_Blur_Divide.png",final_step/K_PCA(dim_2))
 
 
